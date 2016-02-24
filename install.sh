e1c1514a5c6a4cdc10da467e8f15c40c4249fa4e
Gavin Lee <sz110010@gmail.com>
2016-02-25 02:16:47 +0800

    Edit install.sh
    Fix bug

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #install tmux.conf
-ln -fs `pwd`/tmux/.tmux.conf ~/.tmux.conf
-cd `pwd`/tmux/vendor/tmux-mem-cpu-load
+ln -fs ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+cd ~/.dotfiles/tmux/vendor/tmux-mem-cpu-load
 sudo yum install cmake
 sudo pip install psutil
 cmake .
 make
 sudo make install clean
-sudo cp ~/.tmux/vendor/basic-cpu-and-memory.tmux /usr/local/bin/tmux-mem-cpu-load
+sudo cp ~/.dotfiles/tmux/vendor/basic-cpu-and-memory.tmux /usr/local/bin/tmux-mem-cpu-load
 sudo chmod +x /usr/local/bin/tmux-mem-cpu-load
 
 #install screenrc
-ln -fs `pwd`/screenrc ~/.screenrc
+ln -fs ~/.dotfiles/screenrc ~/.screenrc
 
 #install bashrc
-ln -fs `pwd`/bashrc ~/.bashrc
+ln -fs ~/.dotfiles/bashrc ~/.bashrc
