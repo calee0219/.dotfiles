9bfb57ebbeb7c9aa2352b7938dfb73d138cca354
Gavin Lee <sz110010@gmail.com>
2016-03-06 04:13:36 +0800

    Edit app_install.sh
    Add google chrome

--- app_install.sh
+++ app_install.sh
@@ -16,3 +16,9 @@ yum install gcc
 yum install clang
 yum install nodejs
 yum install ruby
+
+# browser
+wget http://chrome.richardlloyd.org.uk/install_chrome.sh
+chmod u+x install_chrome.sh
+./install_chrome.sh
+rm install_chrome.sh
