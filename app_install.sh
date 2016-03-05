1a788ee4c950ef22c0cb4fea62ebdb2ece85b705
Gavin Lee <sz110010@gmail.com>
2016-03-06 03:18:12 +0800

    Edit app_install.sh
    Catergory

--- app_install.sh
+++ app_install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-yum install go
+# useful tool
 yum install git
+yum install tmux
+
+# editor
+yum install vim
+yum install emacs
+
+# language
+yum install go
 yum install python
 yum install gcc
 yum install clang
-yum install tmux
-yum install vim
-yum install emacs
+yum install nodejs
