0b7a5b10b7ae792e0325c9937bbae0971635875e
Gavin Lee <sz110010@gmail.com>
2017-06-18 19:37:48 +0800

    What: Rm gcp sdk, add vim mode on fish
    Why:
    
    How:

--- config.fish
+++ config.fish
@@ -1,8 +1,7 @@
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/calee0219/Downloads/google-cloud-sdk/path.fish.inc' ];
-    if type source > /dev/null; source '/Users/calee0219/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/calee0219/Downloads/google-cloud-sdk/path.fish.inc'; end;
-end
+# global verable
+fish_vi_key_bindings
 
+# command
 function ser
     ssh -X ssh.calee.com.tw -p 50
 end
@@ -19,4 +18,8 @@ function ccp
     #clang++ -std=c++17 -Wall -Wextra -pedantic -g3 -O2 ${*} -o ${1%.*} && time ./${1%.*}
     clang++ -std=c++1z $argv
     time ./a.out
+end
+
+function work
+    ssh -X calee0219@140.110.18.200 -p 9487
 end
